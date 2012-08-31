625bc63d843a4a98d19348fbf364ba4eff5b9947
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-08-31 12:51:44 +0200

    Describe my project

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :description "FIXME: write description")
+  :description "A project to massively rename my folder according to my naming convention.")
