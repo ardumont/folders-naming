34331c9f6a46062291e8db6e82ff37e07245e396
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-08-31 14:46:17 +0200

    Added the needed deps.

--- project.clj
+++ project.clj
@@ -1,7 +1,15 @@
 (defproject folders-naming/folders-naming "0.1.0-SNAPSHOT"
   :license {:name "Eclipse Public License",
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [fs "1.3.2"]]
+  :profiles {:dev
+             {:dependencies
+              [[com.intelie/lazytest
+                "1.0.0-SNAPSHOT"
+                :exclusions
+                [swank-clojure]]
+               [midje "1.4.0"]]}}
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :description "A project to massively rename my folder according to my naming convention.")
