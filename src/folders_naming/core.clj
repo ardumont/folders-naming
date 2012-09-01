051ec0296d569e5bb0629a20cda0cf9a16fb3cf7
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-09-01 13:04:36 +0200

    Rename the files in the arborescence.

--- src/folders_naming/core.clj
+++ src/folders_naming/core.clj
@@ -34,35 +34,93 @@
 (fact "Play with home"
   (fs/absolute-path (fs/home "try")) => "/home/try")
 
-;; taken from https://github.com/Raynes/fs/blob/master/test/fs/core_test.clj
-(defn create-walk-dir []
-  (let [root (fs/temp-dir "fs-")]
-    (fs/mkdir (fs/file root "a"))
-    (fs/mkdir (fs/file root "b"))
-    (spit (fs/file root "1") "1")
-    (spit (fs/file root "a" "2") "1")
-    (spit (fs/file root "b" "3") "1")
-    root))
-
-;; create a temporary directory to walk on!
-(def tmp-test-dir (create-walk-dir))
-
-(fs/walk vector tmp-test-dir)
-;; ([#<File /tmp/fs-1346434699865-1888304452> #{"a" "b"} #{"1"}] [#<File /tmp/fs-1346434699865-1888304452/b> #{} #{"3"}]
-;; [#<File /tmp/fs-1346434699865-1888304452/a> #{} #{"2"}])
-
 ;; ############################ Serious stuff
 
 (def naming-conv (comp str/join
                        (partial interpose \-)
-                       (partial map (comp str/trim str/lower-case))
-                       #(str/split % #"(?<=[a-z])(?=[A-Z\s])")
+                       (partial map (comp str/lower-case
+                                          #(str/replace % #"_" "")
+                                          str/trim))
+                       #(str/split % #"_|\s")
                        str/trim))
 
 (fact
   (naming-conv " no-space ")                        => "no-space"
-  (naming-conv "CamelCase")                         => "camel-case"
-  (naming-conv "CamelCase ")                        => "camel-case"
-  (naming-conv "IDONOTWANTTOSEEUpperCase")          => "idonotwanttoseeupper-case"
-  (naming-conv "I-DoNotWantTo See Blank Space Too") => "i-do-not-want-to-see-blank-space-too")
+  (naming-conv "I-DONOTWANTTOSEEUpperCase")         => "i-donotwanttoseeuppercase"
+  (naming-conv "I-DoNotWantTo See Blank Space Too") => "i-donotwantto-see-blank-space-too"
+  (naming-conv "I-DoNotWantTo_see_underscore_too")  => "i-donotwantto-see-underscore-too"
+  (naming-conv "file_To-RENAME_correctly.ext")      => "file-to-rename-correctly.ext")
+
+(comment "Test the renaming facility!"
+  (fs/mkdir  "/tmp/test")                              ;;true
+  (fs/touch "/tmp/test/0001.xt")                       ;;#<core$file fs.core$file@111ebb3>
+  (fs/rename "/tmp/test/0001.xt" "/tmp/test/0001.txt") ;;true
+  (fs/list-dir "/tmp/test")                            ;;("0001.txt")
+  (fs/rename "/tmp/test" "/tmp/test2")                 ;;true
+  (fs/list-dir "/tmp/test")                            ;;nil
+  (fs/list-dir "/tmp/test2")                           ;;("0001.txt")
+  (fs/mkdir  "/tmp/test")                              ;;true
+  (fs/touch "/tmp/test/0001.xt")                       ;;#<core$file fs.core$file@111ebb3>
+  (fs/rename "/tmp/test/0001.xt" "/tmp/test/0001.txt") ;;true
+  (fs/list-dir "/tmp/test")                            ;;("0001.txt")
+  (fs/rename "/tmp/test" "/tmp/test2")                 ;;true
+  (fs/list-dir "/tmp/test")                            ;;nil
+  (fs/list-dir "/tmp/test2")                           ;;("0001.txt")
+)
+
+(defn rename-file! "Rename a file according to the naming convention"
+  [file]
+  (let [path (. file getAbsolutePath)
+        name (fs/base-name path)
+        nname (naming-conv name);; we can abstract the naming-conv function here!
+        ppath (fs/parent path)
+        npath (str ppath "/" nname)]
+    (println "path" path)
+    (println "npath" npath)
+    (fs/rename path npath)))
+
+(fact "rename-file!"
+  (let [temp-dir (fs/temp-dir (str "fs-"))
+        temp-dir-path (. temp-dir getAbsolutePath)
+        file-for-test (io/file (str temp-dir-path "/file_To-RENAME_correctly.ext"))
+        file-created? (fs/create file-for-test)]
+    (rename-file! file-for-test) => true
+    (fs/list-dir temp-dir-path) => (contains "file-to-rename-correctly.ext")))
+
+(defn do-rename!
+  "A function to use with the fs/walk function.
+  This will walk amongst the tree arborescence and rename folders and files."
+  [current-file dirs files]
+  (if (fs/directory? current-file)
+    (let [current-path (. current-file getAbsolutePath)
+          ren-file! (comp rename-file!
+                          io/file
+                          (partial str current-path "/"))]
+      (println "current-path" current-path)
+      (doall
+       (map ren-file! files)))))
+
+;; taken from https://github.com/Raynes/fs/blob/master/test/fs/core_test.clj
+(defn test-create-walk-dir "Create a temporary folder to test the life, the universe and everything!"
+  []
+  (let [root (fs/temp-dir "fs-")
+        dir-with-underscore (fs/file root "dir_with_underscores")]
+    (fs/mkdir (fs/file root "This is a dir with_blank space"))
+    (fs/mkdir dir-with-underscore)
+    (fs/create (fs/file root "toto with blank space.txt"))
+    (fs/create (fs/file root "ThisIsAFileAndBlank-space.mp4"))
+    (fs/create (fs/file root "This_is_AFileInAnd_underscore and spaces.mp4"))
+    (let [subroot (fs/file (str (. dir-with-underscore getAbsolutePath) "/another_subdirectory"))]
+      (fs/mkdir subroot)
+      (fs/create (fs/file subroot "This is a subfile.another_extension"))
+      root)))
+
+(let [root (test-create-walk-dir)
+      root-path (. root getPath)
+      res (fs/walk do-rename! root-path)]
+  (clojure.pprint/pprint root-path )
+  (clojure.pprint/pprint res);; see the result
+  (clojure.pprint/pprint (fs/iterate-dir root));; see the new arbo
+#_  (fs/delete-dir root);; cleanup afterwards
+  res)
 
