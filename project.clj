5698122df25ad3adaa0fc81ff5e182458782b329
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-09-01 20:42:15 +0200

    Move midje into dev deps.

--- project.clj
+++ project.clj
@@ -2,14 +2,15 @@
   :license {:name "Eclipse Public License",
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [midje "1.4.0"]
-                 [fs "1.3.2"]]
+                 [fs "1.3.2"]
+                 [org.clojure/tools.cli "0.2.1"]]
   :profiles {:dev
              {:dependencies
               [[com.intelie/lazytest
                 "1.0.0-SNAPSHOT"
                 :exclusions
-                [swank-clojure]]]}}
+                [swank-clojure]]
+               [midje "1.4.0"]]}}
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :description "A project to massively rename my folder according to my naming convention.")
